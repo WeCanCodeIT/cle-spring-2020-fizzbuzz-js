d1c9403ce5d5a744a3d7f72423a55a342038b74a
Jen E <jenevans3ten@gmail.com>
2020-03-04 14:43:38 -0500

    moved fizzbuzz to class; used loop to test multiple values; added DOM elements and set values

--- spec/js/fizzbuzz.spec.js
+++ spec/js/fizzbuzz.spec.js
@@ -1,16 +1,78 @@
 describe('Fizz Buzz Game', function(){
 
+    let sut;
+    
+    beforeEach(function(){
+        sut = new FizzBuzz();
+    })
+
     describe('getFizzBuzzValue', function(){
-        it('should return 1 for 1', function(){
+
+        describe('for plain numbers', function(){
+
+            let numberValues = [1, 2, 4, 7, 8, 11];
+
+            numberValues.forEach(num =>{
+                it('should return ' + num + ' for ' + num, function(){
+                    expect(sut.getFizzBuzzValue(num)).toBe(num + '');
+                });
+            })
+        })
+        xit('should return 1 for 1', function(){
             expect(getFizzBuzzValue(1)).toBe('1');
         });
 
-        it('should return 2 for 2', function(){
+        xit('should return 2 for 2', function(){
             expect(getFizzBuzzValue(2)).toBe('2');
         });
 
         it('should return Fizz for 3', function(){
-            expect(getFizzBuzzValue(3)).toBe('Fizz');
+            expect(sut.getFizzBuzzValue(3)).toBe('Fizz');
+        });
+
+        it('should return Buzz for 5', function(){
+            expect(sut.getFizzBuzzValue(5)).toBe('Buzz');
+        });
+
+        it('should return FizzBuzz for 15', function(){
+            expect(sut.getFizzBuzzValue(15)).toBe('FizzBuzz');
+        });
+    })
+
+    describe('DOM element', function(){
+        beforeEach(function(){
+            fizzBuzzButton = document.querySelector('#fizzbuzz-button');
+            fizzBuzzNumber = document.querySelector('#fizzbuzz-number');
+            fizzBuzzValue = document.querySelector('#fizzbuzz-value');
+        })
+
+        describe('button', function(){
+            it('should exist', function(){
+                expect(fizzBuzzButton).not.toBeNull();
+            })
+
+        })
+
+        describe('number container', function(){
+            it('should exist', function(){
+                expect(fizzBuzzNumber).not.toBeNull();
+            })
+
+            it('should initially display 0', function(){
+                sut.setInitialNumber();
+                expect(fizzBuzzNumber.innerText).toBe('0');
+            })
+        })
+
+        describe('fizzbuzz container', function(){
+            it('should exist', function(){
+                expect(fizzBuzzValue).not.toBeNull();
+            })
+
+            it('should initially display 0', function(){
+                sut.setInitialFizzBuzzValue();
+                expect(fizzBuzzValue.innerText).toBe('0');
+            })
         })
     })
 
--- src/js/fizzbuzz.js
+++ src/js/fizzbuzz.js
@@ -1,10 +1,66 @@
-const getFizzBuzzValue = function(number){
-    if(number % 3 === 0){
-        return "Fizz";
+
+class FizzBuzz{
+    
+    constructor(){
+        this.number = 0;
     }
-    return number + "";
+    
+    getFizzBuzzValue(number){
+        if(this.isMultipleOf3(number) && this.isMultipleOf5(number)){
+            return "FizzBuzz";
+        }
+        else if(this.isMultipleOf3(number)){
+            return "Fizz";
+        }
+        else if(this.isMultipleOf5(number)){
+            return "Buzz";
+        }
+        return number + "";
+    }
+    
+    isMultipleOf3(number){
+        if (number % 3 === 0) {
+            return true;
+        }
+        return false;
+        
+    }
+
+    isMultipleOf5(number){
+        if (number % 5 === 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    setInitialNumber(){
+        document.querySelector('#fizzbuzz-number').innerText = "0";
+        console.log('setting number to 0');
+    }
+    
+    setInitialFizzBuzzValue(){
+        document.querySelector('#fizzbuzz-value').innerText = "0";
+    }
+    
 }
 
-// function getFizzBuzzValueVersion2(number){
-//     return number + "";
-// }
+let fizzBuzz = new FizzBuzz();
+fizzBuzz.setInitialNumber();
+fizzBuzz.setInitialFizzBuzzValue();
+
+// const getFizzBuzzValue = function(number){
+    //     if(number % 15 === 0){
+        //         return "FizzBuzz";
+        //     }
+        //     else if(number % 3 === 0){
+            //         return "Fizz";
+            //     }
+            //     else if(number % 5 === 0){
+                //         return "Buzz";
+                //     }
+                //     return number + "";
+                // }
+                
+                // function getFizzBuzzValueVersion2(number){
+                    //     return number + "";
+                    // }
